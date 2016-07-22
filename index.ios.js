00bfef7367157ea5e119442be370acaf184593e4
App Academy Student <student@appacademy.io>
2016-07-22 10:50:20 -0700

    added incomplete redux setup, need xcode

--- index.ios.js
+++ index.ios.js
@@ -1,39 +1,6 @@
 'use strict';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  NavigatorIOS,
-} from 'react-native';
-const Splash = require('./src/containers/splash');
+import React, { AppRegistry } from 'react-native';
+import Main from './src';
 
-class Project extends Component {
-  render() {
-    return (
-        <NavigatorIOS
-          style={styles.container}
-          initialRoute={{
-            component: Splash,
-            title: "Welcome"
-          }}
-        />
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  text: {
-    color: 'black',
-    backgroundColor: 'white',
-    fontSize: 30,
-    margin: 80
-  },
-  container: {
-    flex: 1
-  }
-});
-
-AppRegistry.registerComponent('Project', () => Project);
+AppRegistry.registerComponent('LincolnLogger', () => Main);
